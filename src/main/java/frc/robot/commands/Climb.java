4e68577bc54954f34eca3d5a8d55dac4656d26da
abhiramatluri42 <91764285+abhiramatluri42@users.noreply.github.com>
2022-10-21 22:08:55 -0500

    Update Climb.java

--- src/main/java/frc/robot/commands/Climb.java
+++ src/main/java/frc/robot/commands/Climb.java
@@ -7,11 +7,15 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Robot;
 import frc.robot.RobotContainer;
+import frc.robot.subsystems.Climber;
 
 public class Climb extends CommandBase {
   /** Creates a new climb. */
-  public Climb() {
+  public Climber climber;
+
+  public Climb(Climber climber) {
     // Use addRequirements() here to declare subsystem dependencies.
+    this.climber = climber;
     addRequirements(RobotContainer.climber);
   }
 
